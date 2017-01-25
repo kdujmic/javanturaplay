ae33542cd1d40b0a5d2035af3a47fc8439239b03
Kresimir Dujmic <kresimir.dujmic@etna.hr>
2017-01-25 13:40:27 +0100

    Logging info level, Integration test fixed

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -20,11 +20,12 @@ class ApplicationSpec extends PlaySpec with OneAppPerTest {
   "HomeController" should {
 
     "render the index page" in {
-      val home = route(app, FakeRequest(GET, "/")).get
 
+
+      val home = route(app, FakeRequest(GET, "/speakers")).get
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Your new application is ready.")
+      contentAsString(home) must include ("Javantura Speakers database")
     }
 
   }
